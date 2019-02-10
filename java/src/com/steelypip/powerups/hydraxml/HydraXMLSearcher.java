2ee5e3b253f8f62b9550bcd4854231fe15320f11
Stephen Leach <sfkleach@gmail.com>
2019-02-10 13:57:01 +0000

    Removing superfluous files imported from the initial project import.

--- java/src/com/steelypip/powerups/hydraxml/HydraXMLSearcher.java
+++
@@ -1,9 +0,0 @@
-package com.steelypip.powerups.hydraxml;
-
-/**
- * A convenience class for implementing a recursive walk of a tree
- * with some control over the branches explored.
- */
-public abstract class HydraXMLSearcher extends AbsMultiTreeSearcher< String, HydraXML > implements HydraXMLAsMultiTree {
-	//	Skip.
-}
--- java/src/com/steelypip/powerups/hydraxml/HydraXMLWalker.java
+++
@@ -1,9 +0,0 @@
-package com.steelypip.powerups.hydraxml;
-
-/**
- *	A convenience class that implements a recursive
- * 	walk over a HydraXML tree. 
- */
-public abstract class HydraXMLWalker extends AbsMultiTreeWalker< String, HydraXML > implements HydraXMLAsMultiTree {
-	//	Skip.
-}
