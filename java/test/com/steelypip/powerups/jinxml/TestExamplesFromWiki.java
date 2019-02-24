b43adaa7c2020c93d47a19a8293e821022ba4fb6
Stephen Leach <sfkleach@gmail.com>
2019-02-24 20:37:55 +0000

    Add another example from the wiki.

--- java/test/com/steelypip/powerups/jinxml/TestExamplesFromWiki.java
+++ java/test/com/steelypip/powerups/jinxml/TestExamplesFromWiki.java
@@ -98,4 +98,42 @@ public class TestExamplesFromWiki {
 		assertEquals( "address", e.getChild( "address" ).getName() );
 	}	
 	
+	final static String example5 =  
+		"<widget debug=\"on\">" +
+		"    <window>" +
+		"        title: \"Sample Konfabulator Widget\"," +
+		"        name: \"main_window\"," +
+		"        width: 500," +
+		"        height: 500" +
+		"    </window>" +
+		"    <image>" +
+		"        src: \"Images/Sun.png\"," +
+		"        name: \"sun1\"," +
+		"        hOffset: 250," +
+		"        vOffset: 250," +
+		"        alignment: \"center\"" +
+		"    </image>" +
+		"    <text>" +
+		"        data: \"Click Here\"," +
+		"        size: 36," +
+		"        style: \"bold\"," +
+		"        name: \"text1\"," +
+		"        hOffset: 250," +
+		"        vOffset: 100," +
+		"        alignment: \"center\"," +
+		"        onMouseUp: \"sun1.opacity = (sun1.opacity / 100) * 90;\"" +
+		"    </text>" +
+		"</widget>";
+	
+	@Test
+	public void testExample5() {
+		StdPushParser p = new StdPushParser( new StringReader( example5 ), false );
+		Element e = p.readElement();
+		assertEquals( 3, e.countMembers() );
+		assertEquals( "text", e.getChild( 2 ).getName() );
+	}	
+	
+	
+	
+	
 }
