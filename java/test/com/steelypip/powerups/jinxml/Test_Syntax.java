93558aedc31bfda207c87d56a8c5db7ab6d950ad
Stephen Leach <sfkleach@gmail.com>
2022-11-04 16:51:40 +0000

    Fixed the mismatch of brackets issue

--- java/test/com/steelypip/powerups/jinxml/Test_Syntax.java
+++ java/test/com/steelypip/powerups/jinxml/Test_Syntax.java
@@ -9,13 +9,21 @@ import org.junit.Test;
 public class Test_Syntax {
 
 	final String example1 = ( 
-			"[ </array>\n"
-		);
+		"[ </array>\n"
+	);
 
-		@Test( expected=RuntimeException.class )
-		public void MismatchedBrackets() {
-			Element foo = Element.readElement( new StringReader( example1 ) );
-			assertTrue( foo.hasName( "array" ) );
-		}
+	@Test( expected=RuntimeException.class )
+	public void MismatchedBrackets1() {
+		Element foo = Element.readElement( new StringReader( example1 ) );
+	}
+
+	final String example2 = ( 
+		"{ </object>\n"
+	);
+
+	@Test( expected=RuntimeException.class )
+	public void MismatchedBrackets2() {
+		Element foo = Element.readElement( new StringReader( example2 ) );
+	}
 
 }
