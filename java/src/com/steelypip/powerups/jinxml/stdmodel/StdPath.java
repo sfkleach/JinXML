9af363bb12f4d11c70a57e953ae0d0cf7a18a027
Stephen Leach <sfkleach@gmail.com>
2022-11-09 21:24:45 +0000

    WIP

---
+++ java/src/com/steelypip/powerups/jinxml/stdmodel/StdPath.java
@@ -0,0 +1,56 @@
+package com.steelypip.powerups.jinxml.stdmodel;
+
+import com.steelypip.powerups.jinxml.Element;
+import com.steelypip.powerups.jinxml.Path;
+
+public class StdPath implements Path {
+
+	@Override
+	public Element getElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSelector() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Path getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable< Path > generatePaths() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable< Path > generatePaths( String selector ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAlreadyGenerated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isOwnAncestor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
